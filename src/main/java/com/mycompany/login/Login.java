f21209e5c295ccce245f37c0aa60ffebe71e2f2c
Laucode22 <lau.saltaren96@gmail.com>
2024-04-15 23:34:43 +0300

    update

---
+++ src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.login;
+
+import com.mycompany.login.gui.InitLogin;
+
+public class Login {
+
+    public static void main(String[] args) {
+        InitLogin principal = new InitLogin();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+        
+    }
+}
